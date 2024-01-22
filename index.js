51753cf4e7c082602192673f024b4833f740c409
Stanley Metray <stanleymetray@gmail.com>
2024-01-22 17:41:22 +0530

    Optimised

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const myForm = document.getElementById("myForm").addEventListener("submit", (e) 
     let expenseList = document.getElementById("expenseList");
     const e1 = new Expense(document.getElementById("expenseAmount").value, document.getElementById("description").value, document.getElementById("category").value);
     let expenses = JSON.parse(localStorage.getItem("expenses") || '[]');
-    expenses.push(e1.amount + " - " + e1.description + " - " + e1.category + " " + '<button onClick="editEvent(event)" type="button" class="btn-edit btn btn-secondary me-2">Edit</button><button onClick="deleteEvent(event)" type="button" class="btn-delete btn btn-danger">Delete</button>');
+    expenses.push("<span>" + e1.amount + " - " + e1.description + " - " + e1.category + "</span>" + '<button onClick="editEvent(event)" type="button" class="btn-edit btn btn-secondary me-2">Edit</button><button onClick="deleteEvent(event)" type="button" class="btn-delete btn btn-danger">Delete</button>');
 
     expenseList.innerHTML = "";
-    expenses.forEach((item) => {
+    expenses.forEach((item, index) => {
         let listItem = document.createElement("li");
         listItem.innerHTML = item;
         listItem.classList.add("my-3");
@@ -29,40 +29,65 @@ const myForm = document.getElementById("myForm").addEventListener("submit", (e) 
 function editEvent(event) {
     let expenses = JSON.parse(localStorage.getItem("expenses") || '[]');
     let index = -1;
-    if (expenses) {
-        expenses.forEach((item) => {
-            let arr1 = item.split(" - ");
-            let arr2 = event.target.parentElement.innerHTML.split(" - ")
-            if (arr1[0] === arr2[0] && arr1[1] === arr2[1]) {
-                document.getElementById("expenseAmount").value=arr1[0];
-                document.getElementById("description").value=arr1[1];
-                index++;
-            }
-        });
-    }
 
+    if (expenses) {
+        for (i = 0; i < expenses.length; i++) {
+            let span = event.target.parentElement.children[0];
+            if (expenses[i].indexOf(span.innerText) != -1) {
+                index = i;
+                let values = span.innerText.split(" - ");
+                document.getElementById("expenseAmount").value = values[0];
+                document.getElementById("description").value = values[1];
+                document.getElementById("category").value = values[2];
+                break;
+            }
+        }
+    }
+    
     if (index != -1) {
         expenses.splice(index, 1);
     }
+
+    // list UL elements after removing
+    let expenseList = document.getElementById("expenseList");
+
+    expenseList.innerHTML = "";
+    expenses.forEach((item, index) => {
+        let listItem = document.createElement("li");
+        listItem.innerHTML = item;
+        listItem.classList.add("my-3");
+        expenseList.appendChild(listItem);
+    });
     localStorage.setItem("expenses", JSON.stringify(expenses));
 }
 
 function deleteEvent(event) {
     let expenses = JSON.parse(localStorage.getItem("expenses") || '[]');
     let index = -1;
-    if (expenses) {
-        expenses.forEach((item) => {
-            let arr1 = item.split(" - ");
-            let arr2 = event.target.parentElement.innerHTML.split(" - ")
-            if (arr1[0] === arr2[0] && arr1[1] === arr2[1]) {
-                event.target.parentElement.remove();
-                index++;
-            }
-        });
-    }
 
+    if (expenses) {
+        for (i = 0; i < expenses.length; i++) {
+            let span = event.target.parentElement.children[0];
+            if (expenses[i].indexOf(span.innerText) != -1) {
+                index = i;
+                break;
+            }
+        }
+    }
+    
     if (index != -1) {
         expenses.splice(index, 1);
     }
+
+    // list UL elements after removing
+    let expenseList = document.getElementById("expenseList");
+
+    expenseList.innerHTML = "";
+    expenses.forEach((item, index) => {
+        let listItem = document.createElement("li");
+        listItem.innerHTML = item;
+        listItem.classList.add("my-3");
+        expenseList.appendChild(listItem);
+    });
     localStorage.setItem("expenses", JSON.stringify(expenses));
 }
